1ccdf95baafdf53677b8bb1d4b281b9880ac0389
Slevunio <slevunio@gmail.com>
2019-12-18 23:26:18 +0100

    gcloud postgresql final

--- src/main/resources/db/postgresql/data.sql
+++ src/main/resources/db/postgresql/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO users VALUES (1, 'Slevunio', 'slevunio@gmail.com', 'password', 'admin', '2019-05-01 00:00:00-07', '2019-05-01') ON CONFLICT DO NOTHING;
-INSERT INTO pets VALUES (1, 'Fafik', '2019-05-11 00:00:00', 'Pies', 'M', '3', 'Tak', 'Nie', 'Tak', '8', 'Brak') ON CONFLICT DO NOTHING;
+INSERT INTO roles values (1, 'ADMIN');
+INSERT INTO roles values (2, 'MODERATOR');
+INSERT INTO roles values (3, 'USER');
+INSERT INTO users VALUES (1, 'admin', '', 1, '$2y$12$BGyHZl47JBFlTjoRyD6dMe5kuuF8basf2n7ZGyHWiuIuZLHvN9k2q', '2019-05-01 00:00:00', '2019-05-01 00:00:00');
+
--- src/main/resources/db/postgresql/schema.sql
+++ src/main/resources/db/postgresql/schema.sql
@@ -1,17 +1,24 @@
+
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
-  user_name varchar(30),
-  email varchar(30),
-  password varchar(30),
-  user_role varchar(30),
+  user_name VARCHAR(30),
+  email  VARCHAR(30),
+  role_id INTEGER REFERENCES roles(id),
+  password VARCHAR(60),
   created TIMESTAMPTZ,
-  last_changed TIMESTAMPTZ
+  last_changed TIMESTAMPTZ,
 );
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    role_name varchar(30)
+);
+#ALTER TABLE users ADD CONSTRAINT fk_key_users_roles FOREIGN KEY (role_id) REFERENCES roles (id);
 
 CREATE TABLE IF NOT EXISTS pets (
     id SERIAL PRIMARY KEY,
     pet_name VARCHAR(30),
-    take_in_date TIMESTAMP,
+    take_in_date TIMESTAMPTZ,
+    last_changed TIMESTAMPTZ,
     species VARCHAR(30),
     sex VARCHAR(30),
     age INTEGER,
@@ -19,5 +26,18 @@ CREATE TABLE IF NOT EXISTS pets (
     can_live_with_other_cats VARCHAR(30),
     can_live_with_kids VARCHAR(30),
     activity INTEGER,
-    diseases VARCHAR(150)
+    coat varchar(30),
+    fur varchar(30),
+    is_ill varchar(30),
+    additional_notes longvarchar
 );
+
+CREATE TABLE IF NOT EXISTS photos (
+    id SERIAL PRIMARY KEY,
+    photo_name varchar(30),
+    photo_type varchar(30),
+    photo_data bytea,
+    pet_id INTEGER NOT NULL REFERENCES pets(id)
+);
+
+#ALTER TABLE photos ADD CONSTRAINT fk_photos_pet FOREIGN KEY (pet_id) REFERENCES pets (id);
